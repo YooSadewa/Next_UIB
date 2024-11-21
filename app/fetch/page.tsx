055723ac2286085f459c507c9e35430a8163b9bf
YooSadewa <tugasthyy@gmail.com>
2024-11-21 14:46:43 +0700

    check fetch 2

--- app/fetch/page.tsx
+++ app/fetch/page.tsx
@@ -8,17 +8,15 @@ export default async function Date() {
     return (
         <>
             <h1>Tanggal Merah</h1>
-            <ul className="grid grid-cols-4 gap-4 items-center">
-                {
-                    data?.dayoff?.map( (item : any, index : any) => {
-                        <li className="w-52 mx-4" key={index}>
-                            <div className="p-2 text-sm text-black bg-gray-400">
-                                {item.tanggal} <span className="rounded-lg p-1 px-1 ml-2 bg-gray-200">${item.keterangan}</span>
+                <ul>
+                    {data?.dayoff?.map((item: any, index: number) => (
+                        <li key={index}>
+                            <div>
+                                {item.tanggal} <span>{item.keterangan}</span>
                             </div>
                         </li>
-                    } )
-                }
-            </ul>
+                    ))}
+                </ul>
         </>
     );
 }
